bd8e77b62241d27c2d50f4d7633d2c387267e2d7
Buğra İPEK <bugraipek2003@gmail.com>
2022-04-07 08:46:18 +0300

    first

--- index.js
+++ index.js
@@ -8,3 +8,18 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+  const Discord = require('discord.js');
+  const client = new Discord.Client();
+  
+  client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+  });
+  
+  client.on('message', msg => {
+    if (msg.content === '!bilge') {
+      msg.reply("Bre ayran budalası!"+'<br/>'+"İyice saldın bizi de"+'<br/>'+"Lütfedip de gelirsen"+'<br/>'+"Gelirsen işte o zaman"+'<br/>'+"En nihayetinde her şey olacağına varacak");
+    }
+  });
+  
+  client.login("OTYxMzc4NDUwMDU1MjQxNzg5.Yk4HdA.LxejJ0rhdjojGSbfw-3pe6vqCyU");
